3159fd0bcd06a0877f2057a0fd9589829cd548c9
brian higuita <brian.higuita@hotmail.co.uk>
2018-03-14 15:27:16 +0000

    start : node app.js in package.json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static("public"));
 app.set("view engine", "ejs")
 
@@ -14,8 +16,6 @@ app.get("/results", function(req, res){
   var query = req.query.search
   var url = "http://www.omdbapi.com/?apikey=3cfe2a5f&&t=" + query;
 
-
-
   request(url, function(error, response, body){
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body)
@@ -27,6 +27,6 @@ app.get("/results", function(req, res){
 
 
 
-app.listen(3000, function(){
-  console.log("server has started");
+app.listen(app.get('port'), function() {
+  console.log('app is running on', app.get('port'))
 });
